17347606de261c51b9a23bbf3056ef5a6ccd2dfc
Alec Luna <alecluna@csus.edu>
2019-05-01 16:25:26 -0700

    trying to import json model from tensorflow

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as tf from "../node_modules/@tensorflow/tfjs";
 
-function App() {
+const loadModel = () => {
+  return tf.loadLayersModel(
+    "http://localhost:3000/tensorflowjs_converted/model.json"
+  );
+};
+loadModel().then(model => console.log(model));
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +29,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
