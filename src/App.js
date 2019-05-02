7ad7d8112aed07a1da340665c7a21e532c37504c
Josh Singh <jashmit04@yahoo.com>
2019-05-01 21:56:42 -0700

    Using keras exported model and apply prediction to a string

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import * as tf from "../node_modules/@tensorflow/tfjs";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import * as tf from '../node_modules/@tensorflow/tfjs';
+import jsonData from './word2vec.json';
+const loadJsonData = () => JSON.parse(JSON.stringify(jsonData));
+let word2index = loadJsonData();
 
 const loadModel = () => {
   return tf.loadLayersModel(
-    "http://localhost:3000/tensorflowjs_converted/model.json"
+    'http://localhost:3000/tensorflowjs_converted/model.json',
   );
 };
-loadModel().then(model => console.log(model));
+const sentenceToArray = sentence => {
+  let array = Array(2000).fill(0);
+  for (let word of sentence.split()) {
+    if (word.toLowerCase() in word2index) {
+      array[word2index[word.toLowerCase()]]++;
+    }
+  }
+  return array;
+};
+tf.tidy(() => {
+  loadModel().then(model => {
+    console.log(model);
+    const tfArray = sentenceToArray('sexual sex gay fake crimes');
+    console.log(tf.tensor(tfArray).expandDims(0));
+    model
+      .predict(tf.tensor(tfArray).expandDims(0))
+      .data()
+      .then(predictions => {
+        const resultIdx = tf.argMax(predictions).dataSync()[0];
+        console.log(predictions, resultIdx);
+      });
+  });
+});
 
 const App = () => {
   return (
